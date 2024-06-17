291b387a807ea574ff360fae8ad48eacd51cb9e0
PRADHAN <sayantan.pradhan@capgemini.com>
2024-06-17 16:33:31 +0530

    modified files

--- tf-src/modules/redshift/redshift.tf
+++ tf-src/modules/redshift/redshift.tf
@@ -2,7 +2,7 @@ resource "aws_redshift_cluster" "openweather-redshift-cluster" {
   cluster_identifier = "openweather-redshift-cluster"
   database_name      = "mydb"
   master_username    = "superuser"
-  master_password    = "Password@2024"
+  master_password    = "Password2024"
   node_type          = "dc2.large"
   cluster_type       = "single-node"
   default_iam_role_arn = var.redshift-role-arn
